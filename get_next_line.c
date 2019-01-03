c8c6078a38e380959c747565d104b60b4a5802eb
Yzarack <jordan.thiercelin@gmail.com>
2019-01-03 21:19:49 +0100

    qweqweqwe

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: jthierce <jthierce@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/04 17:17:26 by marvin            #+#    #+#             */
-/*   Updated: 2018/12/14 12:04:56 by jthierce         ###   ########.fr       */
+/*   Updated: 2019/01/03 19:44:25 by jthierce         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ int		ft_strchrint(const char *s, int c)
 	return (i);
 }
 
-char	*ft_printinline(char **line, t_list *tmp)
+void	ft_printinline(char **line, t_list *tmp, char **str)
 {
 	t_list	*debut;
 
@@ -37,14 +37,14 @@ char	*ft_printinline(char **line, t_list *tmp)
 	{
 		if (!(*line = (char *)malloc(sizeof(char) * ((ft_chainlistlen(tmp) - 1)
 		* BUFF_SIZE + ft_strchrint(tmp->content, '\n') + 1))))
-			return (NULL);
+			return ;
 	}
 	else
 	{
 		printf("bombe has been planted\n");
 		if (!(*line = (char *)malloc(sizeof(char) * ((ft_chainlistlen(tmp) - 1)
 		* BUFF_SIZE + ft_strlen(tmp->content)))))
-			return (NULL);
+			return ;
 	}
 	printf("espace mallocé = %zd\n", ((ft_chainlistlen(tmp) - 1)
 	* BUFF_SIZE + ft_strchrint(tmp->content, '\n') + 1));
@@ -54,22 +54,33 @@ char	*ft_printinline(char **line, t_list *tmp)
 	while (tmp != NULL)
 	{
 		if (ft_strchrint(tmp->content, '\n') == -1)
+		{
 			*line = ft_strncat(*line, tmp->content, tmp->content_size);
+			printf("qwe1derkfkkfkfs = %s\n", ft_strncat(*line, tmp->content, tmp->content_size));
+		}
 		else
-			//*line = ft_strncat(*line, tmp->content, ft_strchrint(tmp->content, '\n'));
-			printf("qwe = %s\n", ft_strncat(*line, tmp->content, ft_strchrint(tmp->content, '\n')));
+		{
+			printf("*line = %s\n", *line);
+			printf("size = %d\n", ft_strchrint(tmp->content, '\n'));
+			printf("tmp->content = %s\n", (char *)tmp->content);
+			ft_strncat(*line, tmp->content, ft_strchrint(tmp->content, '\n'));
+			printf("line = %s\n", *line);
+		}
 		tmp = tmp->next;
 		printf("6 dans while\n");
 	}
+	if (!(*str = (char *)malloc(sizeof(char) * (ft_strlen(debut->content) - ft_strchrint(debut->content, '\n')))))
+		return ;
+	*str = ft_strchr(debut->content, '\n');
 	printf("6 fin while\n");
 	printf("line = %s \n", *line);
-	return (ft_strchr(tmp->content, '\n'));
 }
 
 char	*ft_readinlst(const int fd, char **line, const char *save)
 {
 	t_list	*tmp;
 	t_list	*debut;
+	char	*str;
 
 	printf("2\n");
 	if (save == NULL)
@@ -80,7 +91,11 @@ char	*ft_readinlst(const int fd, char **line, const char *save)
 			return (NULL);
 	}
 	else
-		tmp = ft_lstnew(save, ft_strlen(save));
+	{
+		printf("save = %s\n", save);
+		tmp = ft_lstnew(save + 1, ft_strlen(save));
+		printf("ft_readinlst tmp = %s\n", (char *)tmp->content);
+	}
 	debut = tmp;
 	printf("3\n");
 	while (ft_strchr(tmp->content, '\n') == NULL && tmp->content_size != 0)
@@ -96,7 +111,10 @@ char	*ft_readinlst(const int fd, char **line, const char *save)
 		tmp->next = NULL;
 	}
 	printf("fin boucle while 4\n");
-	return (ft_printinline(line, tmp));
+	ft_printinline(line, debut, &str);
+	printf("line verification = %s \n", *line);
+	printf("str = %s\n", str);
+	return (str);
 }
 
 int		get_next_line(const int fd, char **line)
@@ -106,10 +124,12 @@ int		get_next_line(const int fd, char **line)
 
 	if (!(save))
 		save = NULL;
+	printf("testostes SAVE = %s\n", save);
 	tmp = save;
+	printf("testostes TMP = %s\n", tmp);
 	if (fd < 0 || line == NULL || fd > 1024)
 		return (-1);
-	save = ft_readinlst(fd, line, save);
+	save = ft_readinlst(fd, line, tmp);
 	printf("lolilol: %s\n", *line);
 	if (ft_strchr(save, '\n') == NULL)
 		return (0);
--- main.c
+++ main.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jthierce <jthierce@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/12/24 10:05:02 by jthierce          #+#    #+#             */
+/*   Updated: 2019/01/03 20:19:03 by jthierce         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "get_next_line.h"
 #include <fcntl.h>
 #include <stdio.h>
@@ -5,14 +17,19 @@
 int		main(int argc, char **argv)
 {
 	int		fd;
+	int		i;
 	char	*line;
 
+	i = 5;
 	if (argc == 1)
 		fd = 0;
 	else if (argc == 2)
 		fd = open(argv[1], O_RDONLY);
 	else
 		return (2);
-	printf("valeur de renvoi = %d\n", get_next_line(fd, &line));
-	printf("line = %s", line);
+	while (i != 0)
+	{
+	i = get_next_line(fd, &line);
+	printf("--------------------------line = %s\n", line);
+	}
 }
